6151f4f6fbee1a4e90ebe3cc7841213817a4ef0a
davidka2003 <d4v1ds0n.p@gmail.com>
2023-04-13 21:53:46 +0300

    +jwt-authorization

--- apps/main-app/src/users/dto/index.ts
+++ apps/main-app/src/users/dto/index.ts
@@ -1,23 +1,59 @@
+import { PASSWORD_REGEX } from '@app/constants';
 import {
+  IsDateString,
   IsEmail,
   IsNumber,
   IsOptional,
   IsPhoneNumber,
   IsString,
+  IsStrongPassword,
+  Matches,
   Max,
+  MaxDate,
   Min,
+  MinDate,
 } from 'class-validator';
 
 export class CreateUserRequestDto {
+  @Matches(PASSWORD_REGEX, {
+    message:
+      'Password is not strong enough, it should containt at least 1 uppercase, 1 lowercase and 1 number, minimal length is 6',
+  })
+  password: string;
   @IsEmail()
   email: string;
   @IsPhoneNumber()
   @IsOptional()
   phone?: string;
   @IsString()
-  nickname: string;
-  @IsNumber()
-  @Min(1)
-  @Max(120)
-  age: number;
+  username: string;
+  /**
+   * @description birthDate should be in ISO 8601 format i.e 2003-04-01T21:00:00.000Z
+   */
+  @IsDateString()
+  birthDate: Date;
+}
+
+export class LoginWithPasswordDto {
+  @IsString()
+  password: string;
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+  @IsPhoneNumber()
+  @IsOptional()
+  phone?: string;
+  @IsString()
+  @IsOptional()
+  username?: string;
+}
+
+export class UpdateUsersUsernameDto {
+  @IsString()
+  username: string;
+}
+
+export class RefreshAccessTokenDto {
+  @IsString()
+  refreshToken: string;
 }
