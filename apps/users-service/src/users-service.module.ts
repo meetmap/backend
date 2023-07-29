6808e408b4ace93c20705e2ea7997249e752b556
davidka2003 <d4v1ds0n.p@gmail.com>
2023-07-29 04:41:04 +0300

    upd

--- apps/users-service/src/users-service.module.ts
+++ apps/users-service/src/users-service.module.ts
@@ -2,6 +2,7 @@ import { AuthModule } from '@app/auth';
 import { DatabaseModule } from '@app/database';
 import { RabbitmqModule } from '@app/rabbitmq';
 import { S3UploaderModule } from '@app/s3-uploader';
+import { AppTypes } from '@app/types';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { FriendsModule } from './friends/friends.module';
@@ -16,12 +17,14 @@ import { UsersModule } from './users/users.module';
     }),
     DatabaseModule.init({
       connectionStringEnvPath: 'USERS_SERVICE_DATABASE_URL',
-      microserviceName: 'users-service',
+      microserviceName:
+        AppTypes.Other.Microservice.MicroServiceName.USERS_SERVICE,
     }),
     RabbitmqModule.forRoot(),
     // InternalAxiosModule,
     AuthModule.init({
-      microserviceName: 'users-service',
+      microserviceName:
+        AppTypes.Other.Microservice.MicroServiceName.USERS_SERVICE,
     }),
     S3UploaderModule,
     UsersModule,
